119d585e7541cd7d87e6e8f3c980a240b8b56a02
Kiba <hackerkiba@gmail.com>
2011-06-16 16:44:53 -0500

    add login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 DividendReporter::Application.routes.draw do
   resources :stocks
   resources :users
+  resources :user_sessions
+  match 'login' =>  "user_sessions#new", :as => :login
+  match 'logout' => "user_sessions#destroy", :as => :logout
   root :to => 'stocks#index'
 end
