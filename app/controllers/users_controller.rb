dc6adcde764948f750cd1cc6184f1b80a7c44990
Kiba <hackerkiba@gmail.com>
2011-06-16 14:26:39 -0500

    add create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,14 @@ class UsersController < ApplicationController
   def new 
     @user = User.new()
   end
+
+  def create 
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Registration successful"
+      redirect_to root_url
+    else
+      render :action => 'new'
+    end
+  end
 end
