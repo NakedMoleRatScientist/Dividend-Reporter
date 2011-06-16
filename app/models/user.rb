f5a5d237db9a963e71114144883e3236d378cc3e
Kiba <hackerkiba@gmail.com>
2011-06-16 15:51:37 -0500

    add attr accessible, authenticates with sorcery and confirmation validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation
+  authenticates_with_sorcery!
+
+  validates_confirmation_of :password, :on => :create, :message => "should match confirmation"
 end
