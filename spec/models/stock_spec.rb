1d07299f51d958d4f6441be4312d8a741988c864
Kiba <hackerkiba@gmail.com>
2011-06-16 11:11:32 -0500

    start work on spec

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Stock do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {
+      :symbol => "KR",
+      :price => BigDecimal.new('24.00'),
+      :dividend_share => BigDecimal.new('0.46'),
+      :yield => BigDecimal.new("1.75"),
+      :ex_date => Date.now,
+      :pay_date => Date.now + 20.days
+    }
+  end
 end
