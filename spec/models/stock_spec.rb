b01568b427eae69412ccce2e2d08c952497f43ed
Kiba <hackerkiba@gmail.com>
2011-06-16 11:15:01 -0500

    fix date values for :ex_date and :pay_date

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -7,8 +7,8 @@ describe Stock do
       :price => BigDecimal.new('24.00'),
       :dividend_share => BigDecimal.new('0.46'),
       :yield => BigDecimal.new("1.75"),
-      :ex_date => Date.now,
-      :pay_date => Date.now + 20.days
+      :ex_date => DateTime.now,
+      :pay_date => DateTime.now + 20.days
     }
   end
   it "should validates with proper values" do
