dd9ce89bd212c6f424a803f63b11cba9dbe7cc32
Kiba <hackerkiba@gmail.com>
2011-06-16 11:54:52 -0500

    always upcase

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,7 @@
 class Stock < ActiveRecord::Base
   validates :symbol, :presence => true, :length => { :maximum => 5}
+  before_save :symbol_upper
+  def symbol_upper 
+    self.symbol.upcase!
+  end
 end
