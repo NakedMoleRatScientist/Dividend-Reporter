f52ba921746fb683e0896275ff7c8b9273227ca1
Kiba <hackerkiba@gmail.com>
2011-06-16 16:25:28 -0500

    check if StocksController#index exists

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe StocksController do
+  describe "GET 'index'" do
+    it "should be successful" do
+      get 'index'
+      response.should be_success
+    end
+  end
 
 end
