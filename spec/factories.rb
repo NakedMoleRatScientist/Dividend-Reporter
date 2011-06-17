368823724c36aab265cf93ee4c95f80fd94e2678
Kiba <hackerkiba@gmail.com>
2011-06-17 16:15:49 -0500

    define factory stock

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,3 +4,12 @@ Factory.define :user do |u|
   u.password "foobar"
   u.password_confirmation "foobar"
 end
+
+Factory.define :stock do |s|
+  s.symbol "KR"
+  s.price BigDecimal.new('24.00')
+  s.dividend_share => BigDecimal.new('0.46')
+  s.yield => BigDecimal("1.75")
+  s.ex_date DateTime.now + 2.days
+  s.pay_date DateTime.now + 20.days
+end
