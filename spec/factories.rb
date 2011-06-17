6e21a3f292ac6803f90e9a8cbc81d6fbdb5c001f
Kiba <hackerkiba@gmail.com>
2011-06-17 16:17:02 -0500

    get rid of syntax error

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,8 +8,8 @@ end
 Factory.define :stock do |s|
   s.symbol "KR"
   s.price BigDecimal.new('24.00')
-  s.dividend_share => BigDecimal.new('0.46')
-  s.yield => BigDecimal("1.75")
+  s.dividend_share BigDecimal.new('0.46')
+  s.yield BigDecimal("1.75")
   s.ex_date DateTime.now + 2.days
   s.pay_date DateTime.now + 20.days
 end
