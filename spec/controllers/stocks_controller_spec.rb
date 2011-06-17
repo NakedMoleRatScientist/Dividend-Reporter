6af4093e6523c3432bd5b580fa98b8c46d697212
Kiba <hackerkiba@gmail.com>
2011-06-17 15:27:41 -0500

    redirect to root_url

--- spec/controllers/stocks_controller_spec.rb
+++ spec/controllers/stocks_controller_spec.rb
@@ -7,6 +7,17 @@ describe StocksController do
       get 'index'
       response.should be_success
     end
+
+    describe "adding stocks" do
+      describe "for non-logged in user" do
+        it "should deny entry to the new page" do
+          get 'new'
+          response.should redirect_to(root_url)
+        end
+      end
+
+    end
+
   end
 
 end
