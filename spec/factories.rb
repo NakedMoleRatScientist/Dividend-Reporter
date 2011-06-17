6eda3423305b3f92b8ba7574dff978089fbac5a1
Kiba <hackerkiba@gmail.com>
2011-06-17 10:21:51 -0500

    define admin

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,3 +4,11 @@ Factory.define :user do |u|
   u.password "foobar"
   u.password_confirmation "foobar"
 end
+
+Factory.define :admin do |a|
+  u.username "admin"
+  u.email "admin@example.com"
+  u.password "foobar"
+  u.password_confirmation("foobar")
+  u.admin true
+end
