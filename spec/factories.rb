abb5195251a11c2c7e8255cdc48c894111a77c30
Kiba <hackerkiba@gmail.com>
2011-06-17 10:36:54 -0500

    unneccesary complexity

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,11 +4,3 @@ Factory.define :user do |u|
   u.password "foobar"
   u.password_confirmation "foobar"
 end
-
-Factory.define :admin do |a|
-  a.username "admin"
-  a.email "admin@example.com"
-  a.password "foobar"
-  a.password_confirmation("foobar")
-  a.admin true
-end
