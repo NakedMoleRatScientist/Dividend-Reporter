80d14cfde8a3f0ae34c295c720c25ed1a9331c0b
Kiba <hackerkiba@gmail.com>
2011-06-20 10:47:30 -0500

    move date validation functionality from stock to quote model

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,12 @@
 class Quote < ActiveRecord::Base
+  validate :ex_date_cannot_be_more_in_the_future_than_pay_date
+  before_save :symbol_upper
+  def symbol_upper 
+    self.symbol.upcase!
+  end
+
+  def ex_date_cannot_be_more_in_the_future_than_pay_date 
+    errors.add(:ex_date, "can't be further into the future than pay_date") if self.ex_date > self.pay_date
+  end
+
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,12 +1,3 @@
 class Stock < ActiveRecord::Base
   validates :symbol, :presence => true, :length => { :maximum => 5}
-  validate :ex_date_cannot_be_more_in_the_future_than_pay_date
-  before_save :symbol_upper
-  def symbol_upper 
-    self.symbol.upcase!
-  end
-
-  def ex_date_cannot_be_more_in_the_future_than_pay_date 
-    errors.add(:ex_date, "can't be further into the future than pay_date") if self.ex_date > self.pay_date
-  end
 end
