683bfa8c1fc150ebbdb19c116822397b87ef7b7b
Kiba <hackerkiba@gmail.com>
2011-06-20 10:10:39 -0500

    remove cruft

--- db/migrate/20110616153930_create_stocks.rb
+++ db/migrate/20110616153930_create_stocks.rb
@@ -3,12 +3,6 @@ class CreateStocks < ActiveRecord::Migration
     create_table :stocks do |t|
       t.string :symbol
       t.string :company_name
-      t.decimal :price
-      t.decimal :dividend_share
-      t.decimal :yield
-      t.date :ex_date
-      t.date :pay_date
-
       t.timestamps
     end
   end
