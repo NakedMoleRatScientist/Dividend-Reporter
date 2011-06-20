12ac78024061a277682a3cc79335fb6e9de99277
Kiba <hackerkiba@gmail.com>
2011-06-20 10:39:36 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,6 @@ ActiveRecord::Schema.define(:version => 20110617152505) do
   create_table "stocks", :force => true do |t|
     t.string   "symbol"
     t.string   "company_name"
-    t.decimal  "price"
-    t.decimal  "dividend_share"
-    t.decimal  "yield"
-    t.date     "ex_date"
-    t.date     "pay_date"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
