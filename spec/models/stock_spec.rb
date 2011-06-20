1a958f17f634a880d3e6551f63fc139eeec1a9a7
Kiba <hackerkiba@gmail.com>
2011-06-20 10:31:15 -0500

    fix

--- spec/models/stock_spec.rb
+++ spec/models/stock_spec.rb
@@ -4,6 +4,7 @@ describe Stock do
   before(:each) do
     @attr = {
       :symbol => "KR",
+      :company_name => "Kroger Company"
       :price => BigDecimal.new('24.00'),
       :dividend_share => BigDecimal.new('0.46'),
       :yield => BigDecimal.new("1.75"),
@@ -25,8 +26,4 @@ describe Stock do
     toolong.should_not be_valid
   end
 
-  it "should never have ex_date come after pay_date" do
-    ex = Stock.new(@attr.merge(:ex_date => DateTime.now + 21.days))
-    ex.should_not be_valid
-  end
 end
