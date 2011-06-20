2af3eabc1e5e1cd3189b4f1b044306eae2a9db30
Kiba <hackerkiba@gmail.com>
2011-06-20 09:07:41 -0500

    add flash notice

--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -1,5 +1,6 @@
 module UserSessionsHelper
-   def admin_user 
+  def admin_user
+    flash[:notice] = "You do not have admin access."
     redirect_to(root_path) unless current_user.admin?
   end
 end
