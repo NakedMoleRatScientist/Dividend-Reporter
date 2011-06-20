ce1f09771b7c4ab836dc6a3d872fbe3aff79852a
Kiba <hackerkiba@gmail.com>
2011-06-20 11:13:03 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110620151858) do
+ActiveRecord::Schema.define(:version => 20110620161227) do
 
   create_table "quotes", :force => true do |t|
     t.integer  "stock_id"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20110620151858) do
     t.date     "pay_date"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "yield"
   end
 
   create_table "stocks", :force => true do |t|
