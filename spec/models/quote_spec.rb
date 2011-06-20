6b95abb5d2aa7e4d9a59a57011a6a4caacc5cb69
Kiba <hackerkiba@gmail.com>
2011-06-20 10:38:47 -0500

    add spec model

---
+++ spec/models/quote_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe Quote do
+  before(:each) do
+    stock = Factory(:stock)
+    @attr = {
+      :stock_id => stock.id,
+      :price => BigDecimal.new('24.00'),
+      :dividend_share => BigDecimal.new("0.46"),
+      :yield => BigDecimal.new("1.75"),
+      :ex_date => DateTime.now,
+      :pay_date => DateTime.now + 20.days
+    }
+  end
+
+  it "should never have ex_date come after pay_date" do
+    ex = Stock.new(@attr.merge(:ex_date => DateTime.now + 21.days))
+    ex.should_not be_valid
+  end
+
+end
