75820cde7407339063cf3259738d9e79a32e8ba4
Kiba <hackerkiba@gmail.com>
2011-06-20 10:37:36 -0500

    add quote factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -9,8 +9,15 @@ Factory.define :stock do |s|
   s.symbol "KR"
   s.company_name "Kroger Company"
   s.price BigDecimal.new('24.00')
-  s.dividend_share BigDecimal.new('0.46')
-  s.yield BigDecimal("1.75")
-  s.ex_date DateTime.now + 2.days
-  s.pay_date DateTime.now + 20.days
+  
 end
+
+Factory.define :quote do |q|
+  q.stock_id 1
+  q.price BigDecimal.new("24.00")
+  q.dividend_share BigDecimal.new('0.46')
+  q.yield BigDecimal("1.75")
+  q.ex_date DateTime.now + 2.days
+  q.pay_date DateTime.now + 20.days
+end
+
