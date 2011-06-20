9c653259bf98901c155c993e8ee0eee0e398f59d
Kiba <hackerkiba@gmail.com>
2011-06-20 13:40:54 -0500

    fix all kind of bug

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,14 +1,17 @@
 module StocksHelper
   def convert_to_quotes symbols
-    stocks.split!(", ")
-    quotes = YahooStock::Quote.new(:stock_symbols => stock)
+    stocks = stocks.split(", ")
+    stocks.each do |s|
+      quotes = YahooStock::Quote.new(:stock_symbols => s)
+    end
     quotes.standard
     quotes = quotes.result(:to_hash).output
   end
   def become_stocks quotes
-    stocks = {}
+    stocks = []
     quotes.each do |q|
       stocks << { :company_name => q[:name], :symbol => q[:symbol] }
+      puts(stocks[-1])
     end
     stocks
   end
