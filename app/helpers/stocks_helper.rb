49c9b7601ffd6237fb362639238ae2005b341d1f
Kiba <hackerkiba@gmail.com>
2011-06-20 12:08:55 -0500

    add method become_stocks

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,7 +1,16 @@
 module StocksHelper
-  def convert_to_quotes stocks
+  def convert_to_quotes symbols
+    stocks.split!(", ")
     quotes = YahooStock::Quote.new(:stock_symbols => stock)
     quotes.standard
     quotes = quotes.result(:to_hash).output
+  end
+  def become_stocks symbols
+    quotes = convert_to_quotes(symbol)
+    stocks = {}
+    quotes.each do |q|
+      stocks << { :company_name => q[:name], :symbol => q[:symbol] }
+    end
+    stocks
   end
 end
