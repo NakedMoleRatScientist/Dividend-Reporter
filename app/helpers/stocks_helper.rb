3209d31ace0ca16e38daa80321ba769ee53771c4
Kiba <hackerkiba@gmail.com>
2011-06-20 12:03:41 -0500

    converts to quotes

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,2 +1,7 @@
 module StocksHelper
+  def convert_to_quotes stocks
+    quotes = YahooStock::Quote.new(:stock_symbols => stock)
+    quotes.standard
+    quotes = quotes.result(:to_hash).output
+  end
 end
