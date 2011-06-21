18b5d9a4f23c6ab46bf8767d28f3e82b8e6b29df
Kiba <hackerkiba@gmail.com>
2011-06-21 11:30:06 -0500

    they don't belong

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,15 +1,2 @@
 module StocksHelper
-  def convert_to_quotes symbols
-    symbols = symbols.split(", ")
-    quotes = YahooStock::Quote.new(:stock_symbols => symbols)
-    quotes.standard
-    quotes = quotes.results(:to_hash).output
-  end
-  def become_stocks quotes
-    stocks = []
-    quotes.each do |q|
-      stocks << { :company_name => q[:name], :symbol => q[:symbol] }
-    end
-    stocks
-  end
 end
