4fb01c74aaa56ff549a718ced01d5c1a33ee1098
Kiba <hackerkiba@gmail.com>
2011-06-21 11:51:20 -0500

    fix problem with notice not meeting spec by adding a . at the end of flash message

--- app/helpers/user_sessions_helper.rb
+++ app/helpers/user_sessions_helper.rb
@@ -1,5 +1,5 @@
 module UserSessionsHelper
   def admin_user
-    redirect_to(root_path), :notice => "You do not have admin access" unless current_user.admin?
+    redirect_to root_path, :notice => "You do not have admin access." unless current_user.admin?
   end
 end
