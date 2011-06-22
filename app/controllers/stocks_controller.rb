316aeb1dde3415502c369fbf70ac84c3fc893467
Kiba <hackerkiba@gmail.com>
2011-06-22 11:57:05 -0500

    move convert_to_quotes and become_stocks into the QuotesAndStocks module

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,22 +1,9 @@
+require "lib/quotes_and_stock.rb"
 class StocksController < ApplicationController
   before_filter :require_login, :except => [:index]
   before_filter :admin_user, :except => [:show,:index]
 
-  def convert_to_quotes symbols
-    symbols = symbols.split(", ")
-    quotes = YahooStock::Quote.new(:stock_symbols => symbols)
-    quotes.standard
-    quotes = quotes.results(:to_hash).output
-  end
-
-  def become_stocks quotes
-    stocks = []
-    quotes.each do |q|
-      stocks << { :company_name => q[:name], :symbol => q[:symbol] }
-    end
-    stocks
-  end
-
+  include QuotesAndStocks
   def index
     @stocks = Stock.all
   end
