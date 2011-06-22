20f42fe188ebaea87180c88df4631117cb6d1dc6
Kiba <hackerkiba@gmail.com>
2011-06-22 11:53:52 -0500

    module QuotesAndStocks

---
+++ lib/quotes.rb
@@ -0,0 +1,17 @@
+modules QuotesAndStocks
+  def convert_to_quotes symbols
+    symbols = symbols.split(", ")
+    quotes = YahooStock::Quote.new(:stock_symbols => symbols)
+    quotes.standard
+    quotes = quotes.results(:to_hash).output
+  end
+
+  def become_stocks quotes
+    stocks = []
+    quotes.each do |q|
+      stocks << { :company_name => q[:name], :symbol => q[:symbol] }
+    end
+    stocks
+  end
+end
+
