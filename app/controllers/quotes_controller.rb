9497ba96a2c580c7cc0fc11c3b3b1140cb1a9644
Kiba <hackerkiba@gmail.com>
2011-06-22 15:07:46 -0500

    add stand and extend local variable

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,15 @@
 class QuotesController < ApplicationController
   before_filter :require_login
   before_filter :admin_user
-  def create 
+  def create
+    stocks = Stock.all
+    stocks.each do |s|
+      stand = standard_quote(s.symbol)
+      extend = extended_quote(s.symbol)
+      attr = {
+        :stock_id => s.id,
+      }
+    end  
     redirect_to stocks_path
   end
 end
