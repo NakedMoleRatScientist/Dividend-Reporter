a9e5b9b0f9879fe1e4a80c90b1219de0d4b98159
Kiba <hackerkiba@gmail.com>
2011-06-22 16:32:42 -0500

    N/A

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,17 +5,20 @@ class QuotesController < ApplicationController
   def create
     stocks = Stock.all
     stocks.each do |s|
+      breakpoint
       stand = standard_quote(s.symbol)
       extend = extended_quote(s.symbol)
       attr = {
         :stock_id => s.id,
         :price => BigDecimal.new("#{stand[:last_trade_prize_only]}"),
         :dividend_share => BigDecimal.new("#{extend[:dividend_per_share]}"),
-        :yield => BigDecimal.new("#{extend[:dividend_yield]}"),
-        :ex_date => DateTime.parse(extend[:ex_dividend_date]),
-        :pay_date => DateTime.parse(extend[:dividend_pay_date])
+        :yield => BigDecimal.new("#{extend[:dividend_yield]}")
       }
-      stock = Stock.create(attr)
+      unless extend[:dividend_share] == "N/A"
+        attr.merge!({:ex_date => DateTime.parse(extend[:ex_dividend_date]),
+                      :pay_date => DateTime.parse(extend[:dividend_pay_date])})
+      end
+     stock = Stock.create(attr)
     end  
     redirect_to stocks_path
   end
