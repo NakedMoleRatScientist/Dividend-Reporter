941a5023c352105570963fe4550c0d0378a9ff23
Kiba <hackerkiba@gmail.com>
2011-06-22 10:22:08 -0500

    symbol upper

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,10 @@
 class Stock < ActiveRecord::Base
   validates :symbol, :presence => true, :length => { :maximum => 5}
   validates_presence_of :company_name
+
+  before_save :symbol_upper
+  def symbol_upper 
+    symbol.upcase!
+  end
+
 end
