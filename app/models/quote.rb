ed2e643c4472046b946a729a96ac19e68dd48799
Kiba <hackerkiba@gmail.com>
2011-06-22 11:02:27 -0500

    validates the presences of price

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,6 @@
 class Quote < ActiveRecord::Base
   validate :ex_date_cannot_be_more_in_the_future_than_pay_date
-
+  validates :price, :presence => true
   def ex_date_cannot_be_more_in_the_future_than_pay_date 
     errors.add(:ex_date, "can't be further into the future than pay_date") if self.ex_date > self.pay_date
   end
