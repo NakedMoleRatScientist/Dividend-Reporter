8144d541b7d6f34c5ccef69de4a67cd9cfede1f3
Kiba <hackerkiba@gmail.com>
2011-06-22 13:27:06 -0500

    has many quotes

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,7 @@
 class Stock < ActiveRecord::Base
   validates :symbol, :presence => true, :length => { :maximum => 5}
   validates_presence_of :company_name
-
+  has_many :quotes
   before_save :symbol_upper
   def symbol_upper 
     symbol.upcase!
