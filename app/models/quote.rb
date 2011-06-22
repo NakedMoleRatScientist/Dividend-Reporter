d0282a3ec7fec4641ecde8954e64bc696dace75a
Kiba <hackerkiba@gmail.com>
2011-06-22 11:14:14 -0500

    add a validation

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ActiveRecord::Base
   validate :ex_date_cannot_be_more_in_the_future_than_pay_date
   validates :price, :presence => true, :numericality => true
+  validates :dividend_share, :presence => true
   def ex_date_cannot_be_more_in_the_future_than_pay_date 
     errors.add(:ex_date, "can't be further into the future than pay_date") if self.ex_date > self.pay_date
   end
