419884e2dd3ffe87444aefa7c4d813c1045ac808
Kiba <hackerkiba@gmail.com>
2011-06-22 14:31:07 -0500

    rewrite intention

--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -4,7 +4,7 @@ describe QuotesController do
   render_views
   describe "POST 'create'" do
     describe "for non-logged in user" do
-      it "should deny entry to the new page" do
+      it "should ability to create" do
         post :create, :quote => @attr
         response.should redirect_to(root_url)
       end
