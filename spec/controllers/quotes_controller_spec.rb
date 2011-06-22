bfd45d4602dc73841e96b7ef634eef06acb3ea88
Kiba <hackerkiba@gmail.com>
2011-06-22 14:26:12 -0500

    rewrite spec to deal with create, not new

--- spec/controllers/quotes_controller_spec.rb
+++ spec/controllers/quotes_controller_spec.rb
@@ -2,10 +2,10 @@ require "spec_helper"
 
 describe QuotesController do
   render_views
-  describe "GET 'create'" do
+  describe "POST 'create'" do
     describe "for non-logged in user" do
       it "should deny entry to the new page" do
-        get 'new'
+        post :create, :quote => @attr
         response.should redirect_to(root_url)
       end
     end
