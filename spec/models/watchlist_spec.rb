b361a90a9f061cceca26f453f441eb9197d090d2
Kiba <hackerkiba@gmail.com>
2011-06-23 15:46:33 -0500

    rewrite the whole spec to use watched and watching

--- spec/models/watchlist_spec.rb
+++ spec/models/watchlist_spec.rb
@@ -5,7 +5,7 @@ describe Watchlist do
     @watching = Factory(:user)
     @watched = Factory(:stock)
 
-    @watchlist = @watching.watchlists.build(:stock_id => @watched.id, :user_id => @watching.id)
+    @watchlist = @watching.watchlists.build(:watched_id => @watched.id, :watching_id => @watching.id)
   end
 
   it "should create a new instance given valid attributes" do
@@ -28,12 +28,12 @@ describe Watchlist do
 
   describe "validations" do
     it "should require a stock_id" do
-      @watchlist.stock_id = nil
+      @watchlist.watched_id = nil
       @watchlist.should_not be_valid
     end
 
     it "should require a user_id" do
-      @watchlist.user_id = nil
+      @watchlist.watching_id = nil
       @watchlist.should_not be_valid
     end
   end
