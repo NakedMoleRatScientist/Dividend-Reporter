6d641c6fc7afe1e48ab19134396b3363b14226f7
Kiba <hackerkiba@gmail.com>
2011-06-23 16:04:20 -0500

    foreign key watching_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   validates_confirmation_of :password, :on => :create, :message => "should match confirmation"
-  has_many :watchlists, :foreign_key => "user_id"
+  has_many :watchlists, :foreign_key => "watching_id"
   has_many :watching, :through => :watchlists, :source => :watched
 end
