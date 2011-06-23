58542fff5bf2d297cdbdc0db72e91aed45fc733f
Kiba <hackerkiba@gmail.com>
2011-06-23 09:58:09 -0500

    fix syntax error

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -1,7 +1,7 @@
 Given /^I am logged in$/ do
   Factory(:user)
   visit login_path
-  fill_in("username", :with "fooman13")
+  fill_in("username", :with => "fooman13")
   fill_in("password", :with => "foobar")
   click_button("Login")
 end
