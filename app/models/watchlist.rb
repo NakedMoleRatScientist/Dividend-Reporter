08c5333486ac0f1bd5d0b8506f35f57b3861306c
Kiba <hackerkiba@gmail.com>
2011-06-23 14:13:31 -0500

    fix syntax error

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,5 +1,5 @@
 class Watchlist < ActiveRecord::Base
   attr_accessible :watched_id, :watching_id
   belongs_to :watching, :class_name => "User"
-  belongs_to :watched, :class-name => "Stock"
+  belongs_to :watched, :class_name => "Stock"
 end
