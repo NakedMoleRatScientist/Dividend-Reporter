e7e173bedc0375a0b1f79418d3c3d2c2d8a312fe
Kiba <hackerkiba@gmail.com>
2011-06-23 14:34:55 -0500

    has many watchlists

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,7 @@ class Stock < ActiveRecord::Base
   validates :symbol, :presence => true, :length => { :maximum => 5}
   validates_presence_of :company_name
   has_many :quotes, :dependent => :destroy
-
+  has_many :watchlists, :foreign_key => "stock_id"
   before_save :symbol_upper
   def symbol_upper 
     symbol.upcase!
