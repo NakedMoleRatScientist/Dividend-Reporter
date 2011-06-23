dc2a20a2b08556c2b6acfd287d774c2eacdc9294
Kiba <hackerkiba@gmail.com>
2011-06-23 15:35:31 -0500

    validates user id

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,5 +1,7 @@
 class Watchlist < ActiveRecord::Base
-  attr_accessible :watched_id, :watching_id
+  attr_accessible :stock_id, :user_id
   belongs_to :watching, :class_name => "User"
   belongs_to :watched, :class_name => "Stock"
+  validates :stock_id, :presence => true
+  validates :user_id, :presence => true
 end
