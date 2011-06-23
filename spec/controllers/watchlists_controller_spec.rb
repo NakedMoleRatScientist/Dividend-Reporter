3d0c809ca88ff984001436433e306bb0a2c9ee7e
Kiba <hackerkiba@gmail.com>
2011-06-23 16:49:47 -0500

    put the it block inside the describe block for access control

--- spec/controllers/watchlists_controller_spec.rb
+++ spec/controllers/watchlists_controller_spec.rb
@@ -1,8 +1,12 @@
 require "spec_helper"
 
 describe WatchlistsController do
-  it "should require login for create" do
-    post :create
-    response.should redirect_to rooth_path
+  describe "access control" do
+    it "should require login for create" do
+      post :create
+      response.should redirect_to rooth_path
+    end  
   end
+
+  
 end
