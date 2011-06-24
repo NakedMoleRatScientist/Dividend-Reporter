73a9ebac3931544973968ee8ad74382a58e023e7
Kiba <hackerkiba@gmail.com>
2011-06-24 09:45:22 -0500

    implement watchlist user path

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -1,7 +1,9 @@
 class WatchlistsController < ApplicationController
   before_filter :require_login
   def create 
-    
+    @watched = User.find(params[:watchlist][:watched_id])
+    current_user.watch!(@watched)
+    redirect_to watchlist_user_path
   end
 
   def destroy 
