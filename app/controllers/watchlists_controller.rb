4b3440fc128866791ac95de1fb2a71d5445dd01e
Kiba <hackerkiba@gmail.com>
2011-06-24 09:47:17 -0500

    by current_user id

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -3,7 +3,7 @@ class WatchlistsController < ApplicationController
   def create 
     @watched = User.find(params[:watchlist][:watched_id])
     current_user.watch!(@watched)
-    redirect_to watchlist_path
+    redirect_to watchlist_path(current_user.id)
   end
 
   def destroy 
