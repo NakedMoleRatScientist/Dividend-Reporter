a8269a29391a3d67bfcfc30a19a8b3f0c1917320
Kiba <hackerkiba@gmail.com>
2011-06-24 09:35:00 -0500

    switch to watched_id and watching_id

--- app/models/watchlist.rb
+++ app/models/watchlist.rb
@@ -1,7 +1,7 @@
 class Watchlist < ActiveRecord::Base
-  attr_accessible :stock_id, :user_id
+  attr_accessible :watched_id, :watching_id
   belongs_to :watching, :class_name => "User"
   belongs_to :watched, :class_name => "Stock"
-  validates :stock_id, :presence => true
-  validates :user_id, :presence => true
+  validates :watched_id, :presence => true
+  validates :watching_id, :presence => true
 end
